3d1a86c1e4fd2703c1c39876c431427682ec7a86
fernandOlle <frolle@inf.ufpel.edu.br>
2021-09-30 00:15:14 -0300

    Initializing repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Repository for Data Structures and Algorithms III class at Federal University of Pelotas - UFPel
+
+## Demo of complexity difference between exact and approximated algorithms
---
+++ travellingSalesman.py
@@ -0,0 +1,29 @@
+from sys import maxsize
+from itertools import permutations
+from numpy import loadtxt
+import time
+
+def travellingSalesmanProblem(graph, s):
+    start_time =  time.time()
+    V = len(graph)
+    vertex = []
+    for i in range(V):
+        if i != s:
+            vertex.append(i)
+    min_path = maxsize
+    next_permutation=permutations(vertex)
+    for i in next_permutation:
+        current_pathweight = 0
+        k = s
+        for j in i:
+            current_pathweight += graph[k][j]
+            k = j
+        current_pathweight += graph[k][s]
+        min_path = min(min_path, current_pathweight)
+    print("tempo: %s"%(time.time() - start_time))        
+    return min_path
+ 
+if __name__ == "__main__":
+    graphMatrix = loadtxt('tsp_data/tsp3_1194.txt', dtype=int)
+    s = 0
+    print(travellingSalesmanProblem(graphMatrix, s))
